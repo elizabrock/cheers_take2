6ae4e92d76f811a07a460655de6004912aef9444
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-21 13:56:58 -0500

    Initial integration test, with scenario notes.

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -10,4 +10,46 @@ Try again with `./cheers.rb [Name] [MM/DD Birthday]
 EOS
     assert_equal expected, output
   end
+
+  def test_two_valid_arguments
+    skip
+    output = `./cheers Abby 08/25`
+    expected = <<EOS
+Give me an... A
+Give me a... B
+Give me a... B
+Give me a... Y
+Abby’s just GRAND!
+
+Awesome!  Your birthday is in 127 days! Happy Birthday in advance!
+EOS
+    assert_equal expected, output
+  end
+
+  # Scenarios discussed in class:
+  # 0 args -> help message
+  # 1 valid arg
+  # 1 invalid arg
+  # 2 args
+  #   * valid + valid
+  #   * valid + invalid
+  #   * invalid + valid
+  #   * invalid + invalid
+  #
+  # Valid Names:
+  #    * Regular name
+  #    * Name with hyphens
+  # Invalid Names:
+  #   * birthday instead of name
+  #   * all non-word characters
+  #   * "" (empty string)
+  #   * "  " (whitespace)
+  # Valid Months:
+  #   * m/dd (8/25)
+  #   * mm/dd (08/25)
+  #   * birthdays that already happened this year
+  #   * birthdays that haven't happened yet this year
+  # Invalid Months:
+  #   * dd/mm (25/08)
+  #   * dd/mm/yy (08/25/2015)
 end
