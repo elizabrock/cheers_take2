241ea4b52430746ee0470c88fd0036759081b535
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-22 09:19:51 -0500

    Colorize test output

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -1,3 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+require "minitest/reporters"
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
 require 'minitest/autorun'
 
 class TestCheersIntegration < Minitest::Test
